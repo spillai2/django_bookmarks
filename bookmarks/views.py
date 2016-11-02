3ccbeb0fee819c5e0e2e21eb5517ccb2695e1930
Sanjeev Pillai <spillai2@gmail.com>
2016-11-02 10:55:23 -0400

    First commit

---
+++ bookmarks/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
+
+def main_page(request):
+	#template = get_template('main_page.html')
+	variables = Context ({
+		'head_title': 'Django Bookmarks',
+		'page_title': 'Welcome to Django Bookmarks',
+		'page_body': 'Where you can store and share bookmarks!'
+	})
+	#output = template.render(variables) 
+	#return HttpResponse(output)
+	return render(request, 'bookmarks/main_page.html', variables)
